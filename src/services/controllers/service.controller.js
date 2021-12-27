3b2786de2895bb01f5bf4c493b7d071b75bc8287
Andres <andres.fernandezcaballero@davinci.edu.ar>
2021-12-26 23:59:18 -0300

    REMOVE favicon from html on serviceController

--- src/services/controllers/service.controller.js
+++ src/services/controllers/service.controller.js
@@ -10,7 +10,6 @@ const  serviceController = {
             <html lang="es">
             <head>
                 <meta charset="UTF-8">
-                <link rel="shortcut icon" href="https://drive.google.com/file/d/1XsY0NgoJ5KRMMs3poMVJ9UXG5s2MBaI8/view?usp=sharing" type="image/x-icon">
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
                 <title>Servicios hoy</title>
             </head>
