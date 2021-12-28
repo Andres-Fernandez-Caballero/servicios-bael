b267fd361316bfc6418a168e8993ad51094518db
Andres <andres.fernandezcaballero@davinci.edu.ar>
2021-12-27 23:00:02 -0300

    ADD declatation of envs for diferents enviroments ADD microservices flag to control cron-jobs on config file ADD default values in config file

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const app = require('./src/app');
 const appConfig = require("./src/config");
+const cronTasks = require('./src/microservices/cron-tasks/index');
 
+if(appConfig.MICROSERVICES.cornTasksEnabled){
+    cronTasks();
+}
 
 app.listen(appConfig.PORT, () => {
     console.log('servicor escuchando en el puerto ' + appConfig.PORT) })
--- src/config.js
+++ src/config.js
@@ -2,8 +2,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const appConfig = {
-    PORT: process.env.PORT,
-    APP_NAME: 'Servicios Diarios'
+    ENVIRONMENT: process.env.NODE_ENV || 'development', //'production' | 'development' | 'test'
+    PORT: process.env.PORT || 3000,
+    APP_NAME: 'Servicios Diarios',
+    MICROSERVICES: {
+        cornTasksEnabled: process.env.CRON_JOBS || false,
+    }
 }
 
 module.exports = appConfig;
