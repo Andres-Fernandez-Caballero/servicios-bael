56addf441ed3474f70d668bdb12479ac784fd442
Andres <andres.fernandezcaballero@davinci.edu.ar>
2022-01-08 12:43:27 -0300

    MODIFY localStorageDb.js

--- src/database/localstorage/localStorageDb.js
+++ src/database/localstorage/localStorageDb.js
@@ -2,11 +2,10 @@ const LocalStorage = require('node-localstorage').LocalStorage
 const appConfig = require('./../../config');
 
 
-if(appConfig.ENVIRONMENT === 'production') {
-    db = new LocalStorage(`./src/database/localstorage/production`);
-}else{
+let db = new LocalStorage(`./src/database/localstorage/production`);
+
+if(appConfig.ENVIRONMENT !== 'production')
     db = new LocalStorage(`./src/database/localstorage/${appConfig.ENVIRONMENT}`);
-}
 
 
 module.exports = db;
